a2a1c6ada35368ca9269979ddaa5d2d40dad03f2
Mahak kosta <mahakkosta014@gmail.com>
2026-02-05 14:18:20 +0530

    Changed files

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utilis/ExpressError.js");
 const session = require("express-session");
-const MongoStore = require("connect-mongo")(session);
+const MongoStore = require("connect-mongo");
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
@@ -41,17 +41,29 @@ app.use(methodOverride("_method"));
 app.engine('ejs', ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
-const store = new MongoStore({ 
-    url : dbUrl,
-    crypto : {
-        secret : process.env.SECRET,
-    },
-    touchAfter : 24 * 3600,
-})
+// const store = new MongoStore({ 
+//     url : dbUrl,
+//     crypto : {
+//         secret : process.env.SECRET,
+//     },
+//     touchAfter : 24 * 3600,
+// })
 
-store.on("error", () =>{
-    console.log("ERROR IN MONGO SESSION STORE", err)
-})
+// store.on("error", () =>{
+//     console.log("ERROR IN MONGO SESSION STORE", err)
+// })
+
+const store = MongoStore.create({
+    mongoUrl: dbUrl,
+    crypto: {
+        secret: process.env.SECRET,
+    },
+    touchAfter: 24 * 3600,
+});
+
+store.on("error", function (err) {
+    console.log("ERROR IN MONGO SESSION STORE", err);
+});
 
 const sessionOptions = {
     store,
--- controllers/user.js
+++ controllers/user.js
@@ -4,24 +4,61 @@ module.exports.renderSignupForm = (req, res) => {
     res.render("users/signup.ejs");
 };
 
-module.exports.SignupUser = (async (req, res) => {
-    try{
-        let {username, email, password} = req.body;
-        const newUser = new User({username, email});
-        let registeredUser  = await User.register(newUser, password);
-        console.log(registeredUser);
-        req.login(registeredUser,(err) => {
-        if(err) {
-            return next(err);
-        }
+module.exports.SignupUser = async (req, res) => {
+    try {
+        let { username, email, password } = req.body;
+        const newUser = new User({ username, email });
+
+        await User.register(newUser, password);
+
         req.flash("success", "Welcome to Wanderlust");
-        res.redirect("/listings");
-    });
-    }catch(e){
+        return res.redirect("/listings");
+
+    } catch (e) {
         req.flash("error", e.message);
-        res.redirect("/signup");
-    }  
-});
+        return res.redirect("/signup");
+    }
+};
+
+// module.exports.SignupUser = async (req, res, next) => {
+//     try {
+//         let { username, email, password } = req.body;
+//         const newUser = new User({ username, email });
+
+//         let registeredUser = await User.register(newUser, password);
+
+//         req.login(registeredUser, (err) => {
+//             if (err) {
+//                 return next(err);   //  now next exists
+//             }
+//             req.flash("success", "Welcome to Wanderlust");
+//             return res.redirect("/listings"); //  return added
+//         });
+
+//     } catch (e) {
+//         req.flash("error", e.message);
+//         return res.redirect("/signup"); //  return added
+//     }
+// };
+
+// module.exports.SignupUser = (async (req, res, next) => {
+//     try{
+//         let {username, email, password} = req.body;
+//         const newUser = new User({username, email});
+//         let registeredUser  = await User.register(newUser, password);
+//         console.log(registeredUser);
+//         req.login(registeredUser,(err) => {
+//         if(err) {
+//             return next(err);
+//         }
+//         req.flash("success", "Welcome to Wanderlust");
+//         return res.redirect("/listings");
+//     });
+//     }catch(e){
+//         req.flash("error", e.message);
+//         return res.redirect("/signup");
+//     }  
+// });
 
 module.exports.renderLoginForm = (req, res) => {
     res.render("users/login.ejs")
@@ -39,6 +76,6 @@ module.exports.logoutUser =  (req, res, next) => {
             return next(err);
         }
         req.flash("success", "You are logged out");
-        res.redirect("/listings")
+        return res.redirect("/listings")
     })
 }
